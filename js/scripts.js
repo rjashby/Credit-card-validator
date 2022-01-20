dc629b1d3ad41c3be9e57f32553e51e8bfe5e8e6
Ryan Ashby <rjapsu160@gmail.com>
2022-01-20 12:00:48 -0800

    WIP - continue testing 3rd test/method
    Co-Authored-By: Alex McKnight <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,29 @@ function validator2() {
   return newNumber;
 }
 
+// TESTING AREA
+
+function validator2() {
+  const numArray = [1, 2, 3, 5, 5, 3, 2, 1, 1, 2, 3, 5, 5, 3, 2, 1]
+  let newArray = [];
+  numArray.forEach(function(number, i){
+    if ((i) % 2 === 1) {
+      if ((number * 2).length === 2) {
+        let splitNumber = number[i].toString().split("");
+        let newNumber = parseInt(splitNumber[0]) + parseInt(splitNumber[1]);
+        console.log(newNumber);
+        newArray.push(newNumber);
+      } else {
+        newArray.push(number);
+      }
+    } else if ((i) % 2 === 0) {
+      newArray.push(number);
+    }
+  })
+  console.log(newNumber);
+  return newArray;
+}
+
 // USING FOR LOOP - BUGGED
 
 // function validator() {
