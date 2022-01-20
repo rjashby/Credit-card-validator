0c70e64d504ff63c25970a7bdd777a79d54bd2ec
Ryan Ashby <rjapsu160@gmail.com>
2022-01-20 15:04:41 -0800

    add util func turning num to array of nums using .from
    Co-Authored-By: Alex McKnight <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,46 @@
-// FOR EACH LOOP
+// UTILITY FUNCTIONS
 
-function validator2() {
+function numToArray(number) {
+  let myArray = Array.from(String(number), Number);
+  return myArray;
+}
+
+// FOR EACH LOOP METHOD
+
+function validator() {
+  const numArray = [1, 2, 3, 5, 5, 3, 2, 1, 1, 2, 3, 5, 5, 3, 2, 1]
+  let newArray = [];
+  numArray.forEach(function(number, i){
+    if ((i % 2) === 1) {
+      console.log("string length check", (number * 2).length === 2);
+      console.log((number * 2));
+      console.log(typeof number);
+      let newString = (number * 2).toString().split("");
+      if (newString.length === 2) {
+        let newNumber = parseInt(newString[0]) + parseInt(newString[1]);
+        newArray.push(newNumber);
+      } else {
+        newArray.push(number * 2);
+      }
+    } else if ((i % 2) === 0) {
+      newArray.push(number);
+    }
+  })
+  return newArray;
+}
+// INITIAL FUNCTION - NOT ACCOUNTING FOR DOUBLE DIGIT NUMBERS
+
+function validator() {
   const numArray = [1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1]
   let newNumber = [];
   numArray.forEach(function(number, i){
-    if ((i) % 2 === 1) {
+    if (i % 2 === 1) {
       newNumber.push(number * 2);
-    } else if ((i) % 2 === 0) {
+    } else if (i % 2 === 0) {
       newNumber.push(number);
     }
   })
   return newNumber;
-}
-
-// TESTING AREA
-
-function validator2() {
-  const numArray = [1, 2, 3, 5, 5, 3, 2, 1, 1, 2, 3, 5, 5, 3, 2, 1]
-  let newArray = [];
-  numArray.forEach(function(number, i){
-    if ((i) % 2 === 1) {
-      if ((number * 2).length === 2) {
-        let splitNumber = number[i].toString().split("");
-        let newNumber = parseInt(splitNumber[0]) + parseInt(splitNumber[1]);
-        console.log(newNumber);
-        newArray.push(newNumber);
-      } else {
-        newArray.push(number);
-      }
-    } else if ((i) % 2 === 0) {
-      newArray.push(number);
-    }
-  })
-  console.log(newNumber);
-  return newArray;
 }
 
 // USING FOR LOOP - BUGGED
