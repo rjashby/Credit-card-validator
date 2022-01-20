c4d742107a0a5f838e429787d3bd47a44168747e
Ryan Ashby <rjapsu160@gmail.com>
2022-01-20 11:21:32 -0800

    add forEach function and test. passed
    Co-Authored-By: Alex McKnight <alex.mcknight11@gmail.com>

--- README.md
+++ README.md
@@ -6,4 +6,10 @@ Test: "It should take in a 16 digit number and return a number where every other
 Code:
 const number = "1234 4321 1234 4321"
 validator(number)
-Expected Output: "2468 8642 2468 8642"
+Expected Output: "48 62 48 62"
+
+Test: "It should take in a 16 digit number in an array, returning the original number for odd indices, and doubling every number at even indices."
+Code:
+const number = [1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1]
+validator(number)
+Expected Output: [1, 4, 3, 8, 4, 6, 2, 2, 1, 4, 3, 8, 4, 6, 2, 2]
--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-function validator() {
-  numArray = [1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1]
+// FOR EACH LOOP
+
+function validator2() {
+  const numArray = [1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1]
   let newNumber = [];
-  for (let i = 1; i <= numArray.length - 1; i +=2) {
-   newNumber.push(numArray[i] * 2);
-  }
+  numArray.forEach(function(number, i){
+    if ((i) % 2 === 1) {
+      newNumber.push(number * 2);
+    } else if ((i) % 2 === 0) {
+      newNumber.push(number);
+    }
+  })
   return newNumber;
-};
+}
+
+// USING FOR LOOP - BUGGED
+
+// function validator() {
+//   const numArray = [1, 2, 3, 4, 4, 3, 2, 1, 1, 2, 3, 4, 4, 3, 2, 1]
+//   let newNumber = [];
+//   for (let i = 1; i <= numArray.length - 1; i +=2) {
+//    newNumber.push(numArray[i] * 2);
+//   }
+//   return newNumber;
+// };
