cb44928cfd04843742951a48d05944bb19691957
Ryan Ashby <rjapsu160@gmail.com>
2022-01-20 21:30:13 -0800

    WIP - attempt to debug checkCompany function

--- js/scripts.js
+++ js/scripts.js
@@ -9,18 +9,19 @@ let DISCOVER = [6, 7, 8, 9, 6, 7, 8, 9, 6, 7, 8, 9, 6, 7, 8, 9];
 
 // UTILITY FUNCTIONS
 
-function numToArray(number) {
+function numToArray(number) { //  CHANGES DIGITS IN A ROW INTO AN ARRAY
   let myArray = Array.from(String(number), Number);
   console.log(typeof myArray[0]);
   return myArray;
 }
 
-function arrReducer(numberArray) {
+
+function arrReducer(numberArray) { // ADDS ALL DIGITS IN ARRAY AND RETURNS SUM
   const reducer = (previousValue, currentValue) => previousValue + currentValue;
   return numberArray.reduce(reducer);
 }
 
-function checkLastDigit(number) {
+function checkLastDigit(number) { // CHECKS TO SEE IF LAST DIGIT IS A 0 OR NOT
   let numCheck = number % 10;
   if (numCheck === 0) {
     return "Valid Card Number"
@@ -28,23 +29,27 @@ function checkLastDigit(number) {
     return "Invalid Card Number"
 }
 
-// CHECK CREDIT COMPANY AND VALID CARD
+// CHECK CREDIT COMPANY AND VALID CARD USING SPLICE - BUGGED
 
 function checkCompany(numberArray) {
   if (numberArray.length = 16) {
     console.log(numberArray.length);
-    if (numberArray.splice(0, 1) === 4) {
-      console.log(numberArray.splice(0, 1));
+    let spliceSixteen = numberArray.splice(0, 1)
+    let spliceFifteen = numberArray.splice(0, 2)
+    // console.log(spliceSixteen);
+    // console.log(spliceFifteen);
+    if (spliceSixteen === 4) {
+      console.log(spliceSixteen);
       return "You have a valid Visa Card."
-    } else if (numberArray.splice(0, 1) === 5) {
+    } else if (spliceSixteen === 5) {
       return "You have a valid Mastercard."
-    } else if (numberArray.splice(0, 1) === 6) {
+    } else if (spliceSixteen === 6) {
       return "You have a valid Discover Card."
     } else {
       return "Invalid Card Number."
     }
   } else if (numberArray.length = 15) {
-    if (numberArray.splice(0, 2) === 34 || numberArray.splice(0, 2) === 37) {
+    if (spliceFifteen === 34 || spliceFifteen === 37) {
       return "You have a valid American Express Card."
     }
   } else {
@@ -52,12 +57,66 @@ function checkCompany(numberArray) {
   }
 }
 
+// SLICE METHOD ON CHECK COMPANY
+
+function checkCompany(numberArray) {
+  if (numberArray.length = 16) {
+    console.log(numberArray.length);
+    let sliceSixteen = numberArray.slice(0, 1);
+    let sliceFifteen = numberArray.slice(0, 2);
+    console.log(sliceSixteen);
+    console.log(sliceFifteen);
+    if (sliceSixteen === 4) {
+      console.log(sliceSixteen);
+      return "You have a valid Visa Card."
+    } else if (sliceSixteen === 5) {
+      return "You have a valid Mastercard."
+    } else if (sliceSixteen === 6) {
+      return "You have a valid Discover Card."
+    } else {
+      return "Invalid Card Number."
+    }
+  } else if (numberArray.length = 15) {
+    if (sliceFifteen === 34 || sliceFifteen === 37) {
+      return "You have a valid American Express Card."
+    }
+  } else {
+    return "Invalid Card Number."
+  }
+}
+
+// SLICE ATTEMPT 2
+
+function checkCompany(numberArray) {
+  let sliceSixteen = numberArray.slice(0, 1);
+  let sliceFifteen = numberArray.slice(0, 2);
+  console.log(numberArray.length);
+  console.log(sliceSixteen);
+  console.log(sliceFifteen);
+  let result = ""
+  if (numberArray.length = 16 && sliceSixteen === 4) {
+    console.log(numberArray.length);
+    console.log(sliceSixteen);
+    console.log(sliceFifteen);
+    result = "You have a valid Visa Card."
+  } else if (numberArray.length = 16 && sliceSixteen === 5) {
+    result = "You have a valid Mastercard."
+  } else if (numberArray.length = 16 && sliceSixteen === 6) {
+    result = "You have a valid Discover Card."
+  } else if ((numberArray.length = 15 && sliceFifteen === 34) || (numberArray.length = 15 && sliceFifteen === 37)) {
+    result = "You have a valid American Express Card."
+  } else {
+    result = "Invalid Card Number."
+  }
+  return result;
+}
+
 // FOR EACH LOOP METHOD
 
 function validator() {
   const numArray = [1, 2, 3, 5, 5, 3, 2, 1, 1, 2, 3, 5, 5, 3, 2, 1]
   let newArray = [];
-  numArray.forEach(function(number, i){
+  numArray.forEach(function(number, i) {
     if ((i % 2) === 1) {
       console.log("string length check", (number * 2).length === 2);
       console.log((number * 2));
