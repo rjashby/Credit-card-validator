eb71508b2ed02628734105d2f5c22ea7af611b99
Ryan Ashby <rjapsu160@gmail.com>
2022-01-20 16:08:55 -0800

    create sample card# variables, and checkCompany function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// SAMPLE CARD NUMBERS
+
+let AMEX = [3, 4, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let AMEX2 = [3, 7, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let notAMEX = [3, 5, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+let VISA = [4, 4, 5, 6, 4, 4, 5, 6, 4, 4, 5, 6, 4, 4, 5, 6];
+let MASTER = [5, 5, 6, 5, 5, 5, 6, 5, 5, 5, 6, 5, 5, 5, 6, 5];
+let DISCOVER = [6, 7, 8, 9, 6, 7, 8, 9, 6, 7, 8, 9, 6, 7, 8, 9];
+
 // UTILITY FUNCTIONS
 
 function numToArray(number) {
@@ -17,6 +26,28 @@ function checkLastDigit(number) {
     return "Valid Card Number"
   } 
     return "Invalid Card Number"
+}
+
+function checkCompany(numberArray) {
+  if (numberArray.length = 16) {
+    console.log(numberArray.length);
+    if (numberArray.splice(0, 1) === 4) {
+      console.log(numberArray.splice(0, 1));
+      return "You have a valid Visa Card."
+    } else if (numberArray.splice(0, 1) === 5) {
+      return "You have a valid Mastercard."
+    } else if (numberArray.splice(0, 1) === 6) {
+      return "You have a valid Discover Card."
+    } else {
+      return "Invalid Card Number."
+    }
+  } else if (numberArray.length = 15) {
+    if (numberArray.splice(0, 2) === 34 || numberArray.splice(0, 2) === 37) {
+      return "You have a valid American Express Card."
+    }
+  } else {
+    return "Invalid Card Number."
+  }
 }
 
 // FOR EACH LOOP METHOD
